62a8696107f38e99ba2680cd59fad6db5f422668
arpage <straypacket@gmail.com>
2025-04-25 17:12:44 +0000

    Prepare for certs

---
+++ proxy/run-getssl.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source .env
+
+BASE=/srv/usagov-chatbot-pipeline/proxy
+
+echo curl https://github.com/srvrco/getssl/releases/download/v2.49/getssl_2.49-1_all.deb
+echo sudo dpkg -i getssl_2.49-1_all.deb
+
+for server in $OLLAMA_HOSTNAME $CHROMA_HOSTNAME; do
+  echo getssl $server 
+done
