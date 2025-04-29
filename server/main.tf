0f369d6dd45a38d31a57238cba777d3dbc1e2343
arpage <straypacket@gmail.com>
2025-04-29 13:48:00 -0400

    Fix sec groups

--- server/main.tf
+++ server/main.tf
@@ -135,9 +135,10 @@ resource "aws_instance" "system_server_primary" {
   ### permission needed: ec2:ImportKeyPair
   key_name      = aws_key_pair.deployer.key_name
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
-  vpc_security_group_ids = [aws_security_group.allow_https.id,
+  vpc_security_group_ids = [aws_security_group.allow_http.id,
+                            aws_security_group.allow_https.id,
                             aws_security_group.allow_ssh.id,
-                            aws_security_group.allow_ollama.id,
+                            #aws_security_group.allow_ollama.id,
                             aws_security_group.allow_chroma.id,
                             aws_security_group.allow_egress.id] # Associate the security group
   tags = {
@@ -171,6 +172,17 @@ resource "aws_vpc_security_group_egress_rule" "system-sec-group-egress" {
     cidr_ipv4 = "0.0.0.0/0"
 }
 
+### Allowed - But always redirected to HTTPS
+resource "aws_security_group" "allow_http" {
+  name = "http_inbound"
+  description = "Allow inbound on tcp/80"
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "allow_http"
+    Purpose = "HTTP Access"
+  }
+}
+
 resource "aws_security_group" "allow_https" {
   name = "https_inbound"
   description = "Allow inbound on tcp/443"
@@ -178,17 +190,6 @@ resource "aws_security_group" "allow_https" {
   tags = {
     Name = "allow_https"
     Purpose = "HTTPS Access"
-  }
-}
-
-### Allowed - But always redirected to HTTPS
-resource "aws_security_group" "allow_http" {
-  name = "https_inbound"
-  description = "Allow inbound on tcp/80"
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "allow_http"
-    Purpose = "HTTP Access"
   }
 }
 
@@ -210,28 +211,28 @@ resource "aws_security_group" "allow_chroma" {
   }
 }
 
-resource "aws_security_group" "allow_ollama" {
-  name        = "allow_ollama"
-  description = "Allows Ollama traffic"
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "allow_ollama"
-  }
-}
+#resource "aws_security_group" "allow_ollama" {
+#  name        = "allow_ollama"
+#  description = "Allows Ollama traffic"
+#  vpc_id = aws_vpc.vpc.id
+#  tags = {
+#    Name = "allow_ollama"
+#  }
+#}
 
-resource "aws_vpc_security_group_ingress_rule" "system-sec-group-ingress-https" {
-    count = length(var.ingress_cidr_blocks)
-    security_group_id = aws_security_group.allow_https.id
-    from_port = 443
-    to_port = 443
-    ip_protocol = "tcp"
-    cidr_ipv4 = var.ingress_cidr_blocks[count.index].cidr_block
-}
 resource "aws_vpc_security_group_ingress_rule" "system-sec-group-ingress-http" {
     count = length(var.ingress_cidr_blocks)
     security_group_id = aws_security_group.allow_http.id
     from_port = 80
     to_port = 80
+    ip_protocol = "tcp"
+    cidr_ipv4 = var.ingress_cidr_blocks[count.index].cidr_block
+}
+resource "aws_vpc_security_group_ingress_rule" "system-sec-group-ingress-https" {
+    count = length(var.ingress_cidr_blocks)
+    security_group_id = aws_security_group.allow_https.id
+    from_port = 443
+    to_port = 443
     ip_protocol = "tcp"
     cidr_ipv4 = var.ingress_cidr_blocks[count.index].cidr_block
 }
@@ -252,14 +253,14 @@ resource "aws_vpc_security_group_ingress_rule" "system-sec-group-ingress-chroma"
     cidr_ipv4 = var.ingress_cidr_blocks[count.index].cidr_block
 }
 
-resource "aws_vpc_security_group_ingress_rule" "system-sec-group-ingress-ollama" {
-    count = length(var.ingress_cidr_blocks)
-    security_group_id = aws_security_group.allow_ollama.id
-    from_port = 11434
-    to_port = 11434
-    ip_protocol = "tcp"
-    cidr_ipv4 = var.ingress_cidr_blocks[count.index].cidr_block
-}
+#resource "aws_vpc_security_group_ingress_rule" "system-sec-group-ingress-ollama" {
+#    count = length(var.ingress_cidr_blocks)
+#    security_group_id = aws_security_group.allow_ollama.id
+#    from_port = 11434
+#    to_port = 11434
+#    ip_protocol = "tcp"
+#    cidr_ipv4 = var.ingress_cidr_blocks[count.index].cidr_block
+#}
 
 #resource "aws_ebs_volume" "system_volume_srv" {
 #  availability_zone = var.availability_zone
